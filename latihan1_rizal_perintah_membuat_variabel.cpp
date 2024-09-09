e79f8f3f64aa0e75831859a450747346cc565573
fiyori101 <muhammadrizalmuhaimin1082@gmail.com>
2024-09-09 19:38:28 +0700

    Add files via upload

---
+++ latihan1_rizal_perintah_membuat_variabel.cpp
@@ -0,0 +1,16 @@
+//latihan 1
+//Dibuat oleh Muahammad Rizal Muhaimin
+//Tanggal dibuat 2 september 20024
+
+#include<iostream>
+
+using namespace std; //Perntah untuk membuat variabel std
+
+int main()
+{
+	cout<<"STMIK PENUSA LUBUK PAKAM" <<endl;
+	cout<<"ALGORITMA PEMROGRAMN"<<endl;
+	cout<<5<<endl;
+	cout <<'A'<<endl;
+	return 0;
+}
