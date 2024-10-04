3864bff680ad6e05141c8fb6db947f1c907d4aeb
fiyori101 <muhammadrizalmuhaimin1082@gmail.com>
2024-10-04 17:28:43 +0700

    Add files via upload

---
+++ latihan18_penggunaan_array.cpp
@@ -0,0 +1,26 @@
+lude <iomanip>
+
+using namespace std;
+
+int main()
+{	int nilai[10], total =0; //kenapa disebut 1 dimensi karna rumus ini menggunakan ([]) 1 balok
+#inc
+	float ratarata;
+	
+	//input nilai
+	for (int i = 0;i <10; i++){
+		cout << "masukkan nilai ke- "<<i +1<< ": ";
+		cin >>nilai [i];
+		total += nilai[i]; //menghitung total
+		//total = total + nilai [i]
+		
+	}
+	//menghitung rata rata
+	ratarata= total/ 10.0;
+	
+	//menampilkan hasil
+	cout << "total nilai : "<< total<< endl;
+	cout << "rata-rata nilai : "<<ratarata<<endl;
+	
+	return 0;
+}
